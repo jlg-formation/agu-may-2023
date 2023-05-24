5a69dc7ad5fd74f549343c64a1a016f2dbc94717
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-24 16:00:22 +0200

    dev: refactored button in async button component

--- front/src/app/widget/async-btn/async-btn.component.ts
+++ front/src/app/widget/async-btn/async-btn.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {
+  IconDefinition,
+  faCircleNotch,
+} from '@fortawesome/free-solid-svg-icons';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-async-btn',
   templateUrl: './async-btn.component.html',
-  styleUrls: ['./async-btn.component.scss']
+  styleUrls: ['./async-btn.component.scss'],
 })
 export class AsyncBtnComponent {
+  @Input()
+  icon: IconDefinition = faCircleNotch;
 
+  @Input()
+  title: string = '';
+
+  @Input()
+  action: Observable<void> = of(undefined);
 }
--- front/src/app/widget/widget.module.ts
+++ front/src/app/widget/widget.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AsyncBtnComponent } from './async-btn/async-btn.component';
-
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AsyncBtnComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    AsyncBtnComponent
-  ]
+  declarations: [AsyncBtnComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [AsyncBtnComponent],
 })
-export class WidgetModule { }
+export class WidgetModule {}
