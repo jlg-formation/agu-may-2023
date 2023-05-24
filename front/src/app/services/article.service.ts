e0431e8b47f33bf138bbd5e2649c5fc7127236ad
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-24 11:44:01 +0200

    dev: use behaviorsubject observable

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Article } from '../interfaces/article';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
+  private articles: Article[] = [
     { id: 'a1', name: 'Tournevis', price: 2.99, qty: 123 },
     { id: 'a2', name: 'Marteau', price: 8, qty: 23 },
   ];
+
+  articles$ = new BehaviorSubject<Article[]>(this.articles);
   constructor() {}
 }
