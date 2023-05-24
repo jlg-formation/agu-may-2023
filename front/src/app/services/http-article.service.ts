b36bfd7ec823f7c89b535f47893ad655be7be58f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-24 17:13:12 +0200

    dev: added articles from back

---
+++ front/src/app/services/http-article.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { ArticleService } from './article.service';
+import { Observable, catchError, map, of, switchMap, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Article } from '../interfaces/article';
+
+const url = 'http://localhost:3000/api/articles';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpArticleService extends ArticleService {
+  constructor(private http: HttpClient) {
+    super();
+    console.log('http article instantiated');
+  }
+
+  override refresh(): Observable<void> {
+    return of(undefined).pipe(
+      switchMap(() => {
+        return this.http.get<Article[]>(url);
+      }),
+      catchError((err) => {
+        console.log('err: ', err);
+        throw new Error('Erreur Technique');
+      }),
+      map((articles) => {
+        this.articles$.next(articles);
+        return;
+      })
+    );
+  }
+}
