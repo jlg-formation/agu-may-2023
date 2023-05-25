43242d0682b7c9f6fb9af170c07c908e03750747
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-25 16:27:57 +0200

    dev: ellipsis test coverage and functional

--- front/src/app/widget/ellipsis.pipe.spec.ts
+++ front/src/app/widget/ellipsis.pipe.spec.ts
@@ -5,4 +5,24 @@ describe('EllipsisPipe', () => {
     const pipe = new EllipsisPipe();
     expect(pipe).toBeTruthy();
   });
+  it('should return the same string', () => {
+    const pipe = new EllipsisPipe();
+    const result = pipe.transform('coucou', 20);
+    expect(result).toBe('coucou');
+  });
+  it('should return cou...', () => {
+    const pipe = new EllipsisPipe();
+    const result = pipe.transform('coucou', 3);
+    expect(result).toBe('cou...');
+  });
+  it('should return an error', () => {
+    const pipe = new EllipsisPipe();
+    let shouldGoHere = false;
+    try {
+      pipe.transform(34, 3);
+    } catch (err) {
+      shouldGoHere = true;
+    }
+    expect(shouldGoHere).toBe(true);
+  });
 });
