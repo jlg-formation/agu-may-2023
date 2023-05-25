c133554143dad49ecb5b420b45107ffef13a3450
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-25 17:04:41 +0200

    dev: added url rewriting

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
     loadChildren: () =>
       import('./stock/stock.module').then((m) => m.StockModule),
   },
+  {
+    path: '**',
+    redirectTo: '/',
+  },
 ];
 
 @NgModule({
