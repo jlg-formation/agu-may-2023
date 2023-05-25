df8764ec89bff3ffd6e50f8ecfbf0410b84aa205
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-25 11:21:25 +0200

    dev: added select mode

--- front/src/app/widget/autofocus.directive.ts
+++ front/src/app/widget/autofocus.directive.ts
@@ -1,12 +1,19 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: 'input[appAutofocus]',
 })
 export class AutofocusDirective implements OnInit {
+  @Input('appAutofocus')
+  mode: 'select' | '' = '';
+
   constructor(private readonly elt: ElementRef<HTMLInputElement>) {}
 
   ngOnInit(): void {
+    if (this.mode === 'select') {
+      this.elt.nativeElement.select();
+      return;
+    }
     this.elt.nativeElement.focus();
   }
 }
