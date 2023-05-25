39a44a9d1516bd40d70d6f23582d20738f8be90a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-05-25 16:00:56 +0200

    dev: added stock coverage 100%

--- front/src/app/stock/stock.component.spec.ts
+++ front/src/app/stock/stock.component.spec.ts
@@ -1,8 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing';
 
 import { StockComponent } from './stock.component';
 import { WidgetModule } from '../widget/widget.module';
+import { throwError } from 'rxjs';
+import { a1 } from 'src/test/data';
+import { Article } from '../interfaces/article';
 
 describe('StockComponent', () => {
   let component: StockComponent;
@@ -21,4 +29,46 @@ describe('StockComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load', fakeAsync(() => {
+    component.load().subscribe();
+    tick(300);
+    expect(component).toBeTruthy();
+  }));
+
+  it('should load in error', fakeAsync(() => {
+    component['articleService'].refresh = () => {
+      return throwError(() => {
+        return new Error('oups');
+      });
+    };
+    component.load().subscribe();
+    tick(300);
+    expect(component).toBeTruthy();
+  }));
+
+  it('should refresh', fakeAsync(() => {
+    component.refresh().subscribe();
+    tick(300);
+    expect(component).toBeTruthy();
+  }));
+
+  it('should remove', fakeAsync(() => {
+    component.selectedArticles = new Set<Article>([a1]);
+    component.remove().subscribe();
+    tick(300);
+    expect(component).toBeTruthy();
+  }));
+
+  it('should select', fakeAsync(() => {
+    component.selectedArticles = new Set<Article>([]);
+    component.select(a1);
+    expect(component.selectedArticles.has(a1)).toBe(true);
+  }));
+
+  it('should unselect', fakeAsync(() => {
+    component.selectedArticles = new Set<Article>([a1]);
+    component.select(a1);
+    expect(component.selectedArticles.has(a1)).toBe(false);
+  }));
 });
